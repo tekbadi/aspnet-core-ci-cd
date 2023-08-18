b8603d07f2b04e7ab32b703e197723be1fcf02fb
tekbadi <121496775+tekbadi@users.noreply.github.com>
2023-08-18 17:23:33 +0000

    initial

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace aspnet_core_ci_cd;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
